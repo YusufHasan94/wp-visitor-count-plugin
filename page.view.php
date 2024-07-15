c827183fdd76a005ce5809247b21dd5c45067e83
yusuf hasan <yhpolok@gmail.com>
2024-07-15 16:03:03 +0600

    add graph to display visitor statistics

--- page.view.php
+++ page.view.php
@@ -1,11 +1,28 @@
 <?php
     $dailyVisitorCounts = $this->get_daily_visitor_count();
+    $dataPoints = array();
+
+    foreach($dailyVisitorCounts  as $count){
+        $date = strtotime($count['date'])*1000;
+        $dataPoints[] = array(
+            "x" => $date, 
+            "y" => $count['visitor_count']
+        );
+    }
+
 ?>
 <style>
     .doremon-counter-main-container{
+        display: flex;
+        justify-content: space-between;
+        margin: 0 50px;
+    }
+    .doremon-counter-label-container{
+        width: 50%;
         padding: 20px 0;
         display: flex;
-        justify-content: center;
+        flex-direction: column;
+        align-items: center;
         gap: 20px;
     }
     .doremon-counter-container{
@@ -28,7 +45,7 @@
         text-align: end;
     }
     .doremon-counter-table-container{
-        width: 100%;
+        width: 50%;
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -37,7 +54,7 @@
         text-transform: capitalize;
     }
     .doremon-counter-table-container table{
-        width: 50%; 
+        width: 100%; 
         font-size: 22px;
         border-collapse: collapse;
     }
@@ -48,59 +65,107 @@
     .doremon-counter-table-container tr{
         text-align: center;
     }
+    #chartContainer{
+        margin: 50px 100px 100px 100px;
+        min-height: 400px;
+    }
+    .canvasjs-chart-credit{
+        display: none!important;
+    }
 </style>
+
+<script>
+    window.onload = function () {
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            exportEnabled: true,
+            theme: "light2", // "light1", "light2", "dark1", "dark2"
+            title:{
+                text: "Visitor Statistics"
+            },
+            axisX:{
+                title: "Date",
+                valueFormatString: "DD MMM YY"
+            },
+            axisY:{
+                title: "Visitor",
+                includeZero: true
+            },
+            data: [{
+                type: "column",
+                xValueType: "dateTime",
+                dataPoints: <?php echo json_encode($dataPoints); ?>
+            }]
+        });
+        chart.render();
+
+    }
+</script>
+
 
 <div>
     <h1>
         Doremon Visitor counter
     </h1>
     <div class="doremon-counter-main-container">
-        <div class="doremon-counter-container doremon-counter-today-visitor">
-            <h1>
-                <span>
-                    Today Visitor
-                </span>
-                <span>
-                    <?php
-                        if(!empty($dailyVisitorCounts)){
-                            $todayVisitor = end($dailyVisitorCounts)['visitor_count'];
-                            echo $todayVisitor;
-                        }
-                    ?>
-                </span>
-            </h1>
+        <div class="doremon-counter-label-container">
+            <div class="doremon-counter-container doremon-counter-today-visitor">
+                <h1>
+                    <span>
+                        Today Visitor
+                    </span>
+                    <span>
+                        <?php
+                            if(!empty($dailyVisitorCounts)){
+                                $date = date('Y-m-d');
+                                $todayVisitor = array_search($date, array_column($dailyVisitorCounts, 'date'));
+                                if($todayVisitor !== false){
+                                    $todayVisitor = $dailyVisitorCounts[$todayVisitor]['visitor_count'];
+                                    echo $todayVisitor;
+                                }else{
+                                    $todayVisitor = 0;
+                                    echo $todayVisitor;
+                                }
+                            }
+                        ?>
+                    </span>
+                </h1>
+            </div>
+    
+            <div class="doremon-counter-container doremon-counter-total-visitor">
+                <h1>
+                    <span>
+                        Total Visitor
+                    </span>
+                    <span>
+                        <?= $this->get_total_visitor_count(); ?>
+                    </span>
+                </h1>
+            </div>
         </div>
-
-        <div class="doremon-counter-container doremon-counter-total-visitor">
-            <h1>
-                <span>
-                    Total Visitor
-                </span>
-                <span>
-                    <?= $this->get_total_visitor_count(); ?>
-                </span>
-            </h1>
-        </div>
-    </div>
-    <div class="doremon-counter-table-container">
-        <h1>visitor statistics</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Visitor</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($dailyVisitorCounts as $dailyCount):?>
+        <div class="doremon-counter-table-container">
+            <h1>visitor Table</h1>
+            <table>
+                <thead>
                     <tr>
-                        <td><?=$dailyCount['date'];?></td>
-                        <td><?=$dailyCount['visitor_count'];?></td>
+                        <th>Date</th>
+                        <th>Visitor</th>
                     </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php foreach($dailyVisitorCounts as $dailyCount):?>
+                        <tr>
+                            <td><?=$dailyCount['date'];?></td>
+                            <td><?=$dailyCount['visitor_count'];?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
+    <div id="chartContainer"></div>
 </div>
+<script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
 
 
