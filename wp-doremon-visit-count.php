80712ea408ee0fb036fcf14198a6e0ece7026516
yusuf hasan <yhpolok@gmail.com>
2024-07-27 16:33:58 +0600

    adding view count column for all pages and posts table

--- wp-doremon-visit-count.php
+++ wp-doremon-visit-count.php
@@ -1,47 +1,56 @@
 <?php
 /*
-Plugin Name: Doremon visitor count
-Description: Count visitor using simple doremon visitor plugin.
+Plugin Name: Doremon view count
+Description: Count view using simple doremon view plugin.
 Version: 1.0.0
 Author: Yusuf
 */
 
-class DoremonVisitorCount{
+class DoremonviewCount{
     public function __construct(){
-        add_action( 'wp', array($this, 'increment_visitor_count'));
-        add_action('wp', array($this, 'track_page_visitor'));
-        add_action( 'admin_menu', array($this, 'add_visitor_menu_page'));
+        add_action( 'wp', array($this, 'increment_view_count'));
+        add_action('wp', array($this, 'track_page_view'));
+        add_action( 'admin_menu', array($this, 'add_view_menu_page'));
+    
+        add_filter('manage_pages_columns', array($this, 'add_viewcount_page_column'));
+        add_action('manage_pages_custom_column', array($this, 'populate_viewcount_page_column'), 20, 2);
+        
+        add_filter('manage_posts_columns', array($this, 'add_viewcount_post_column'));
+        add_action('manage_posts_custom_column', array($this, 'populate_viewcount_post_column'), 10, 2);
+
     }
-    // Function to increment visitor count
-    public function increment_visitor_count() {
+
+    // increment view count
+    public function increment_view_count() {
         if ( ! is_admin() ) {
-            $count = get_option( 'visitor_count', 0 );
-            update_option( 'visitor_count', ++$count );
+            $count = get_option( 'view_count', 0 );
+            update_option( 'view_count', ++$count );
 
             $today = date('Y-m-d');
-            $dailyCounts = get_option('daily_visitor_counts', array());
+            $dailyCounts = get_option('daily_view_counts', array());
             if(isset($dailyCounts[$today])){
                 $dailyCounts[$today] = (int)$dailyCounts[$today]++;
             }else{
                 $dailyCounts[$today] = 1;
             }
-            update_option('daily_visitor_counts', $dailyCounts);
+            update_option('daily_view_counts', $dailyCounts);
         }
     }
-    // Function to display visitor count
-    public function get_total_visitor_count() {
-        $count = get_option( 'visitor_count', 0 );
+    // display view count
+    public function get_total_view_count() {
+        $count = get_option( 'view_count', 0 );
         return $count;
     }
 
-    public function get_daily_visitor_count(){
-        $dailyCounts = get_option('daily_visitor_counts', array());
+    // fetching daily view count
+    public function get_daily_view_count(){
+        $dailyCounts = get_option('daily_view_counts', array());
         $today = date('Y-m-d');
         $totalDayCounts = array();
         if(isset($dailyCounts[$today])){
             $totalDayCounts[] = array(
                 'date'=> $today,
-                'visitor_count'=> $dailyCounts[$today]
+                'view_count'=> $dailyCounts[$today]
             );
         }
         foreach($dailyCounts as $date=>$count){
@@ -50,14 +59,14 @@ class DoremonVisitorCount{
             }
             $totalDayCounts [] = array(
                 'date' => $date,
-                'visitor_count'=>$count
+                'view_count'=>$count
             );
         }
         return $totalDayCounts;
     } 
 
     //allow to count single page view
-    public function track_page_visitor(){
+    public function track_page_view(){
         if(is_singular()){
             global $post;
             $post_id = $post->ID;
@@ -72,7 +81,8 @@ class DoremonVisitorCount{
         }
     }
 
-    public function display_page_visitor() {
+    //display page wise number of view
+    public function display_page_view() {
         global $post;
         if ( isset($post->ID) ) {
             $post_id = $post->ID;
@@ -85,6 +95,7 @@ class DoremonVisitorCount{
         }
     } 
 
+    //display all posts and pages title
     public function title_table_content() {
         global $wpdb; 
         $args = array(
@@ -98,12 +109,12 @@ class DoremonVisitorCount{
                 while ($query->have_posts()) {
                     $query->the_post();
                     ++$count;
-                    $visitor_count = $this->display_page_visitor();
+                    $view_count = $this->display_page_view();
                     ?>
                         <tr>
                             <td style="text-align:center;"><?=$count ?></td>
                             <td><?=get_the_title()?></td>
-                            <td style="text-align:center;"><?=$visitor_count?></td>
+                            <td style="text-align:center;"><?=$view_count?></td>
                         </tr>
                     <?php
                 }
@@ -113,35 +124,63 @@ class DoremonVisitorCount{
         }
     }
 
+    // adding view count column at pages page. 
+    public function add_viewcount_page_column($columns){
+        $columns['view_count'] = __('view count', 'doremon-view-count');
+        return $columns;
+    }  
+
+    // display view count for each page
+    public function populate_viewcount_page_column($column,$post_id){
+        if($column == 'view_count'){
+            $count = get_post_meta($post_id, 'page_visits', true);
+            echo $count?$count:0;
+        }
+    }
+    
+    // adding view count column at posts page.
+    public function add_viewcount_post_column($columns){
+        $columns['view_count'] = __('view count', 'doremon-view-count');
+        return $columns;
+    }  
+
+    // display view count for each post
+    public function populate_viewcount_post_column($column, $post_id){
+        if($column == 'view_count'){
+            $count = get_post_meta($post_id, 'page_visits', true);
+            echo $count? $count:0;
+        }
+    }
 
     // Function to add menu page
-    public function add_visitor_menu_page() {
+    public function add_view_menu_page() {
         add_menu_page(
-            __('Doremon View Count', 'doremon-visitor-count'),              // Page title
-            __('Doremon View Count', 'doremon-visitor-count'),              // Menu title
+            __('Doremon View Count', 'doremon-view-count'),              // Page title
+            __('Doremon View Count', 'doremon-view-count'),              // Menu title
             'manage_options',                                               // Capability required to access
             'doremon_view_count_menu',                                      // Menu slug
-            array($this,'display_visitor_general_page'),                    // Callback function to display page content
+            array($this,'display_view_general_page'),                    // Callback function to display page content
             'dashicons-chart-bar',                                          // Icon (optional)
             4                                                               //priority    
         );
         add_submenu_page(
             'doremon_view_count_menu',                                       // parent slug
-            __('Single page view', 'doremon-visitor-count'),                 // Page title
-            __('Single page view', 'doremon-visitor-count'),                 // Menu title
+            __('Single page view', 'doremon-view-count'),                 // Page title
+            __('Single page view', 'doremon-view-count'),                 // Menu title
             'manage_options',                                                // Capability required to access
             'single_page_view',                                              // Menu slug
             array($this,'display_singular_view_page'),                       // Callback function to display page content
         );
     }
     // Callback function to display menu page content
-    public function display_visitor_general_page() {
+    public function display_view_general_page() {
         require "page.view.php";
     }
+    //callback function to display single page view content
     public function display_singular_view_page() {
         require "singlepage.view.php";
     }
 
 }
 
-$doremonvisitor = new DoremonVisitorCount();
+$doremonview = new DoremonviewCount();
